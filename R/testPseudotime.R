56ef3e41320289e418cbcb897abf4d2dab70181b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-10-15 00:25:22 -0700

    Support blocking factors in testPseudotime, closes #1.

--- R/testPseudotime.R
+++ R/testPseudotime.R
@@ -5,19 +5,18 @@
 #'
 #' @param x A numeric matrix-like object containing log-expression values for cells (columns) and genes (rows).
 #' Alternatively, a \linkS4class{SummarizedExperiment} containing such a matrix.
-#' @param pseudotime A numeric matrix with one row per cell in \code{x} and one column per path (i.e., lineage).
-#' A vector is treated the same as a 1-column matrix.
+#' @param pseudotime A numeric vector of length equal to the number of columns of \code{x}.
 #' @param df Integer scalar specifying the degrees of freedom for the splines.
 #' @param get.lfc Logical scalar indicating whether to return an overall log-fold change along each path.
 #' @param get.spline.coef Logical scalar indicating whether to return the estimates of the spline coefficients.
 #' @param trend.only Logical scalar indicating whether only differences in the trend should be considered
 #' when testing for differences between paths.
 #' @param ... For the generic, further arguments to pass to specific method.
-#'
-#' For the ANY method, further arguments to pass to \code{\link[scuttle]{fitLinearModel}}.
 #' 
 #' For the SummarizedExperiment method, further arguments to pass to the ANY method.
 #' @param assay.type String or integer scalar specifying the assay containing the log-expression matrix.
+#' @param block Factor of length equal to the number of cells in \code{x}, specifying the blocking factor.
+#' @param BPPARAM A BiocParallelParam object from the \pkg{BiocParallel} package, used to control parallelization.
 #'
 #' @return
 #' A \linkS4class{DataFrame} is returned containing the statistics for each gene (row),
@@ -28,45 +27,33 @@
 #' or the differences in the spline fits to the first path are returned (multiple paths).
 #' 
 #' @details
-#' For a single path in \code{pseudotime},
-#' this function fits a natural spline to the expression of each gene with respect to pseudotime.
+#' Tis function fits a natural spline to the expression of each gene with respect to pseudotime.
 #' It then does an ANOVA to test whether any of the spline coefficients are non-zero.
 #' In this manner, genes exhibiting a significant (and potentially non-linear) trend
 #' with respect to the pseudotime can be detected as those with low p-values.
 #' 
-#' For multiple paths in \code{pseudotime}, 
-#' the null hypothesis is that all paths have the same trend (if \code{trend.only=TRUE})
-#' or the same trend and intercept (if \code{FALSE}).
-#' This is done by effectively fitting a separate trend to each path 
-#' and performing an ANOVA to detect differences in the trend alone or in the trend and intercept.
-#' In this manner, genes exhibiting differences in behavior between paths can be detected.
-#'
-#' The expected format of \code{pseudotime} is the same as that returned by \code{\link{orderCells}}.
-#' Each cell is assigned to a path if it has a non-\code{NA} value in the corresponding column.
-#' For single path testing, cells with \code{NA} values in \code{pseudotime} are ignored;
-#' for multiple path testing, cells assigned to multiple paths are ignored.
+#' For trajectories with multiple paths, only one path should be tested at a time.
+#' This usually involves passing a single column of the matrix returned from \code{\link{orderCells}}.
+#' Cells with \code{NA} values in \code{pseudotime} are assumed to be assigned to a different path and are ignored.
 #'
 #' By default, estimates of the spline coefficients are not returned as they are difficult to interpret.
 #' Rather, a log-fold change of expression along each path is estimated
 #' to provide some indication of the overall magnitude and direction of any change.
+#'
+#' \code{block} can be used to fit a separate linear model to each of multiple batches,
+#' after which the statistics are combined across batches as described in \code{\link[scran]{testLinearModel}}.
+#' This avoids potential confounding effects from batch-specific differences in the distribution of cells across pseudotime.
 #' 
 #' @author Aaron Lun
 #'
 #' @examples
 #' y <- matrix(rnorm(10000), ncol=100)
-#'
-#' # Testing for a difference in a single path:
 #' u <- runif(100)
 #' testPseudotime(y, u)
 #'
-#' # Testing for differences in multiple paths
-#' # by mocking up a pseudotime matrix.
-#' p <- cbind(path1=u, path2=u)
-#' path1 <- rbinom(length(u), 1, 0.5)==0
-#' p[!path1,1] <- NA
-#' p[path1,2] <- NA
-#' 
-#' testPseudotime(y, p)
+#' # Handling a blocking factor.
+#' b <- gl(2, 50)
+#' testPseudotime(y, u, block=b)
 #'
 #' @seealso
 #' \code{\link{orderCells}}, to generate the pseudotime matrix.
@@ -76,22 +63,59 @@
 #' @name testPseudotime
 NULL
 
-.test_pseudotime <- function(x, pseudotime, df=5, get.lfc=TRUE, get.spline.coef=FALSE, trend.only=TRUE) {
-    if (is.null(dim(pseudotime)) || ncol(pseudotime)==1) {
-        .test_solo_pseudotime(x, pseudotime, df=df, get.lfc=get.lfc, 
-            get.spline.coef=get.spline.coef)
-    } else {
-        .test_multi_pseudotime(x, pseudotime, df=df, get.lfc=get.lfc, 
-            get.spline.coef=get.spline.coef, trend.only=trend.only)
+#' @importFrom stats p.adjust
+.test_pseudotime <- function(x, pseudotime, df=5, get.lfc=TRUE, get.spline.coef=FALSE, trend.only=TRUE, block=NULL, BPPARAM=NULL) {
+    output <- beachmat::rowBlockApply(x, FUN=.test_blocked_pseudotime, grid=TRUE, BPPARAM=BPPARAM, 
+        pseudotime=pseudotime, df=df, get.lfc=get.lfc, get.spline.coef=get.spline.coef, block=block)
+
+    output <- do.call(rbind, output)
+
+    # Fixing FDRs due to row-level processing.
+    output$FDR <- p.adjust(output$p.value, method="BH")
+    for (i in seq_along(output$per.block)) {
+        output$per.block[[i]]$FDR <- p.adjust(output$per.block[[i]]$p.value, method="BH")
     }
+
+    output
 }
 
 #' @importFrom stats model.matrix
-.test_solo_pseudotime <- function(x, pseudotime, df, get.lfc, get.spline.coef) {
+#' @importFrom S4Vectors metadata
+.test_blocked_pseudotime <- function(x, pseudotime, ..., block) {
     pseudotime <- drop(pseudotime)
 
+    if (is.null(block)) {
+        .test_solo_pseudotime(x, pseudotime=pseudotime, ...)
+    } else {
+        by.block <- split(seq_len(ncol(x)), block)
+        ncells <- lengths(by.block)
+        valid <- logical(length(by.block))
+
+        for (b in seq_along(by.block)) {
+            current <- by.block[[b]]
+            output <- .test_solo_pseudotime(x[,current,drop=FALSE], pseudotime=pseudotime[current], ...)
+            by.block[[b]] <- output
+            resid.df <- metadata(output)$residual.df
+            valid[b] <- !is.na(resid.df) && resid.df > 0L
+        }
+
+        scran::combineBlocks(
+            by.block,
+            method="z", 
+            geometric=FALSE,
+            equiweight=FALSE, 
+            weights=ncells,
+            ave.fields=setdiff(colnames(by.block[[1]]), c("p.value", "FDR")),
+            pval.field="p.value", 
+            valid=valid
+        )
+    }
+}
+
+.test_solo_pseudotime <- function(x, pseudotime, df, get.lfc, get.spline.coef) {
     keep <- !is.na(pseudotime)
-    pseudotime <- pseudotime[keep]
+    pseudotime. <- pseudotime[keep]
+
     x <- x[,keep,drop=FALSE] 
     design <- .forge_spline_basis_design(pseudotime, df=df)
     output <- scran::testLinearModel(x, design=design, coefs=2:ncol(design))
@@ -124,84 +148,6 @@ NULL
     basis <- basis[match(p, up),,drop=FALSE]
 
     cbind(Intercept=rep(1, length(p)), basis)
-}
-
-#' @importFrom stats model.matrix
-.test_multi_pseudotime <- function(x, pseudotime, df, get.lfc, get.spline.coef, trend.only) {
-    if (anyDuplicated(colnames(pseudotime))) {
-        warning("'pseudotime' has duplicated column names")
-        colnames(pseudotime) <- NULL
-    }
-    if (is.null(colnames(pseudotime))) {
-        colnames(pseudotime) <- sprintf("path%i", seq_len(ncol(pseudotime)))
-    }
-
-    nonna <- !is.na(pseudotime)
-    solo <- rowSums(nonna) == 1
-    if (!all(solo)) {
-        x <- x[,solo,drop=FALSE]
-        pseudotime <- pseudotime[solo,,drop=FALSE]
-    }
-
-    common <- rowMeans(pseudotime, na.rm=TRUE)
-    design.raw <- .forge_spline_basis_design(common, df=df)
-
-    all.x <- all.design <- all.lfc <- list()
-    for (i in colnames(pseudotime)) {
-        current.pseudo <- pseudotime[,i]
-        keep <- !is.na(current.pseudo) 
-        current.pseudo <- current.pseudo[keep]
-
-        all.x[[i]] <- x[,keep,drop=FALSE] 
-        cur.design <- design.raw[keep,,drop=FALSE] 
-        colnames(cur.design) <- paste0(colnames(cur.design), ".", i)
-        all.design[[i]] <- cur.design
-
-        if (get.lfc) {
-            design.lfc <- model.matrix(~current.pseudo)
-            all.lfc[[paste0("logFC.", i)]] <- scuttle::fitLinearModel(all.x[[i]], design=design.lfc)$coefficients[,2]
-        }
-    }
-
-    # Creating the design matrix.
-    x2 <- do.call(cbind, all.x)
-    for (i in seq_along(all.design)) {
-        copy <- rep(all.design[i], length(all.design))
-        for (j in setdiff(seq_along(copy), i)) {
-            copy[[j]][] <- 0
-        }
-        all.design[[i]] <- do.call(cbind, copy)
-    }
-    design <- do.call(rbind, all.design)
-
-    # Building up a mega contrast of doom.
-    contrastor <- list()
-    for (i in seq_len(ncol(pseudotime) - 1)) {
-        N <- df + 1L
-
-        con <- matrix(0, ncol(design), N)
-        diag(con) <- -1
-        con[cbind(i * N + seq_len(N), seq_len(N))] <- 1
-        if (trend.only) {
-            con <- con[,-1,drop=FALSE]
-        }
-
-        colnames(con) <- sprintf("spline%i.%s", seq_len(ncol(con)), colnames(pseudotime)[i+1])
-        contrastor[[i]] <- con
-    }
-
-    contrast <- do.call(cbind, contrastor)
-    output <- scran::testLinearModel(x2, design=design, contrasts=contrast)
-
-    # Organizing the output.
-    if (!get.spline.coef) {
-        output <- output[,setdiff(colnames(output), colnames(contrast))]
-    }
-    if (get.lfc) {
-        output <- cbind(DataFrame(all.lfc, row.names=rownames(output)), output)
-    }
-
-    output
 }
 
 #' @export
--- tests/testthat/test-test.R
+++ tests/testthat/test-test.R
@@ -1,6 +1,8 @@
 # This tests the testPseudotime functionality.
 # library(testthat); library(TSCAN); source("test-test.R")
 
+set.seed(01001)
+
 test_that("basic tests work as expected", {
     y <- matrix(rnorm(10000), ncol=100)
     u <- runif(100)
@@ -10,43 +12,16 @@ test_that("basic tests work as expected", {
 
     out <- testPseudotime(y, u, get.spline.coef=TRUE)
     expect_type(out$spline1, "double")
-})
 
-test_that("multi-path tests work as expected", {
-    y <- matrix(rnorm(10000), ncol=100)
-    u <- runif(100)
-    
-    p <- cbind(u, u)
-    path1 <- rbinom(length(u), 1, 0.5)==0
-    p[!path1,1] <- NA
-    p[path1,2] <- NA
+    test <- rbind(rnorm(100))
+    x <- rnorm(100)
+    ref <- testPseudotime(test, x)
+    expect_true(ref$p.value > 0.01)
 
-    out <- testPseudotime(y, p)
-    expect_type(out$logFC.path1, "double")
-    expect_type(out$logFC.path2, "double")
-
-    sout <- testPseudotime(y, p, get.spline.coef=TRUE)
-    expect_type(sout$spline1.path2, "double")
-
-    # Removal of the cells in the shared path has no effect.
-    p2 <- p
-    p2[1:50,] <- runif(50)
-    ref <- testPseudotime(y[,-(1:50)], p2[-(1:50),])
-    out2 <- testPseudotime(y, p2)
-    expect_identical(ref, out2)
-
-    # Respects column names in 'p'.
-    colnames(p) <- c("A", "B")
-
-    out <- testPseudotime(y, p)
-    expect_type(out$logFC.A, "double")
-    expect_type(out$logFC.B, "double")
-
-    out <- testPseudotime(y, p, get.spline.coef=TRUE)
-    expect_type(out$spline1.B, "double")
-
-    colnames(p) <- c("B", "B")
-    expect_warning(testPseudotime(y, p), "duplicated")
+    test <- rbind(jitter(1:100))
+    x <- jitter(1:100)
+    ref <- testPseudotime(test, x)
+    expect_true(ref$p.value < 0.01)
 })
 
 test_that("handles large numbers of duplicated pseudotime values", {
@@ -54,4 +29,34 @@ test_that("handles large numbers of duplicated pseudotime values", {
     u <- c(numeric(50), runif(50))
     expect_error(out <- testPseudotime(y, u), NA)
     expect_error(out <- testPseudotime(y, u, df=51), "not enough unique")
+})
+
+test_that("tests handle blocking", {
+    y <- matrix(rnorm(10000), ncol=100)
+    u <- runif(100)
+    b <- sample(3, ncol(y), replace=TRUE)
+
+    out <- testPseudotime(y, u, block=b)
+
+    expect_identical(length(out$per.block), 3L)
+
+    a1 <- testPseudotime(y[,b==1], u[b==1])
+    a2 <- testPseudotime(y[,b==2], u[b==2])
+    a3 <- testPseudotime(y[,b==3], u[b==3])
+    expect_equivalent(out$per.block[,1], a1)
+    expect_identical(out$p.value, 
+        scran::combinePValues(a1$p.value, a2$p.value, a3$p.value, method="z", weights=table(b)))
+
+    # Works with parallelization.
+    library(BiocParallel)
+    alt <- testPseudotime(y, u, block=b, BPPARAM=SnowParam(2))
+    expect_identical(out, alt)
+
+    # With and without blocking works as expected.
+    test <- rbind(jitter(b))
+    x <- jitter(b)
+    ref <- testPseudotime(test, x)
+    expect_true(ref$p.value <= 0.01)
+    out <- testPseudotime(test, x, block=b)
+    expect_true(out$p.value > 0.1)
 })
